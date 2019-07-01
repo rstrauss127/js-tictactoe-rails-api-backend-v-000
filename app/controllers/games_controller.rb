4fac5a7048d56467c94674ee6b277c777587c702
Rebecca Strauss <rstrauss127@gmail.com>
2019-07-01 22:13:31 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,22 @@
 class GamesController < ApplicationController
   # Add your GamesController code here
+  def create
+    @game = Game.create(game_params)
+  end
 
+  def show
+    @game = Game.find(params[:id])
+    render json: @game
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  private
+  def game_params
+    params.permit(state: [])
+  end
 end
