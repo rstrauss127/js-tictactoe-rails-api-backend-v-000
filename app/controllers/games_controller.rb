a27dad58b1b82a2c71504b1c1c659516bb11a0a2
Rebecca Strauss <rstrauss127@gmail.com>
2019-07-01 22:53:38 +0000

    Automatically backed up by Learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,7 @@ class GamesController < ApplicationController
   # Add your GamesController code here
   def create
     @game = Game.create(game_params)
-    render json: game, status: 201
+    render json: @game, status: 201
   end
 
   def show
